0d12b7d3f6702e0e5b01cb05e8d30cc75ee7159e
Danilo Teixeira <daniloteixeira-santos@hotmail.com>
2013-07-31 18:49:47 -0300

    conversion in jQuery plugin

--- js/slide.js
+++ js/slide.js
@@ -2,7 +2,7 @@
  * @Class : Slide
  * @File : slide.js
  * @Date : 31/07/2013
- * @Version : 0.01
+ * @Version : 0.02
  * @Author : Danilo Teixeira
  * @GitHub : https://github.com/danilo-teixeira
  * MIT license see http://opensource.org/licenses/MIT
@@ -12,12 +12,35 @@
 
 	"use strict";
 
-	function Slide() {
+	var slide = null;
+	var slides = [];
+	var contId = 0;
 
-		this.init();
+	$.fn.extend( {
+
+		"slide" : function() {
+			
+			$.each( this, function() {
+
+				slide = new Slide( this );
+
+			} );
+
+		}
+
+	} );
+
+	function Slide( element ) {
+
+		this.init( $( element ) );
+
+		slides.push( this );
+		contId++;
 
 	}
 
+	Slide.prototype.contId = 0;
+	Slide.prototype.slides = [];
 	Slide.prototype.self = null;
 	Slide.prototype.selfElement = null;
 	Slide.prototype.widthSelf = 0;
@@ -25,10 +48,15 @@
 	Slide.prototype.lenImages = 0;
 	Slide.prototype.indice = 0;
 
-	Slide.prototype.init = function() {
+	/*
+	 * @param {String} element
+	 */
+	Slide.prototype.init = function( element ) {
 
-		Slide.prototype.self = this;
-		this.selfElement = $( "#content-slide" );
+		this.self = this;
+		this.contId = contId;
+		this.slides = slides;
+		this.selfElement = element;
 		this.widthSelf = this.selfElement.width();
 		this.lenImages = this.getImages().length;
 		this.CLASS_SELETOR_NAV = "seletor-nav";
@@ -56,7 +84,7 @@
 
 	Slide.prototype.addNav = function() {
 
-		var navSlide = $( "<ul id='nav-slide'/>" ).css( {
+		var navSlide = $( "<ul id='nav-slide-" + this.contId + "'/>" ).css( {
 			"position" : "absolute",
 			"left" : "10px",
 			"bottom" : "10px",
@@ -91,12 +119,12 @@
 			"cursor" : "pointer"
 		}
 
-		var navLeft = $( "<div id='nav-left-slide'/>" ).css( cssNavRightLeft )
+		var navLeft = $( "<div id='nav-left-slide-" + this.contId + "'/>" ).css( cssNavRightLeft )
 		.on( "click", this.prev );
 
 		$.fn.extend( cssNavRightLeft, { "left" : "auto", "right" : "-35px" } );
 
-		var navRight = $( "<div id='nav-right-slide'/>" ).css( cssNavRightLeft )
+		var navRight = $( "<div id='nav-right-slide-" + this.contId + "'/>" ).css( cssNavRightLeft )
 		.on( "click", this.next );
 		
 		this.selfElement.append( navRight );
@@ -108,9 +136,10 @@
 
 		var images = this.getImages();
 
-		var contentImages = $( "<ul id='content-images'/>" ).css( {
+		var contentImages = $( "<ul id='content-images-" + this.contId + "'/>" ).css( {
 			"height" : "100%",
-			"width" : this.widthSelf * this.lenImages,"left" : "0",
+			"width" : this.widthSelf * this.lenImages,
+			"left" : "0",
 			"top" : "0",
 			"margin" : "0",
 			"padding" : "0",
@@ -144,42 +173,50 @@
 
 	}
 
-	Slide.prototype.prev = function() {
+	Slide.prototype.prev = function( e ) {
 
-		if( slide.self.indice == 0 ) {
+		var element = slides[ e.target.id.replace( /[\D]+/g, "" ) ];
+
+		if( element.self.indice == 0 ) {
 
 			return false;
 
 		}
 		
-		slide.self.indice--;
+		element.self.indice--;
 
-		slide.self.move();
+		element.self.move();
 
 	}
 
-	Slide.prototype.next = function() {
+	Slide.prototype.next = function( e ) {
 
-		if( slide.self.indice == slide.lenImages - 1 ) {
+		var element = slides[ e.target.id.replace( /[\D]+/g, "" ) ];
+
+		if( element.self.indice == element.lenImages - 1 ) {
 
 			return false;
 
 		}
 
-		slide.self.indice++;
+		element.self.indice++;
 
-		slide.self.move();
+		element.self.move();
 
 	}
 
 	Slide.prototype.move = function() {
 
-		$( "#content-images" ).animate( {
-			"margin-left" : -this.indice * this.widthSelf
-		}, 1000, "easeInBack" );
+		$.each( slides, function( i, element) {
+
+			$( "#content-images-" + element.contId ).animate( {
+				"margin-left" : -element.indice * element.widthSelf
+			}, 1000, "easeInBack" );
+
+		} );
 
 	}
 
-	window.slide = new Slide();
+	$( "#content-slide-1, #content-slide-2" ).slide();
 
 } )( jQuery );
